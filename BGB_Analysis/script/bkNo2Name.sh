b345b050ce6ed6ee8742ca2382cb21eddb3ad50d
Eliran Wong <hkwebtech@gmail.com>
2017-05-08 22:54:52 +0100

    update

---
+++ BGB_Analysis/script/bkNo2Name.sh
@@ -0,0 +1,67 @@
+#! /usr/bin/sed -E -i .bak -f
+s/『10』/Genesis/g
+s/『20』/Exodus/g
+s/『30』/Leviticus/g
+s/『40』/Numbers/g
+s/『50』/Deuteronomy/g
+s/『60』/Joshua/g
+s/『70』/Judges/g
+s/『80』/Ruth/g
+s/『90』/1 Samuel/g
+s/『100』/2 Samuel/g
+s/『110』/1 Kings/g
+s/『120』/2 Kings/g
+s/『130』/1 Chronicles/g
+s/『140』/2 Chronicles/g
+s/『150』/Ezra/g
+s/『160』/Nehemiah/g
+s/『190』/Esther/g
+s/『220』/Job/g
+s/『230』/Psalm/g
+s/『240』/Proverbs/g
+s/『250』/Ecclesiastes/g
+s/『260』/Song of Solomon/g
+s/『290』/Isaiah/g
+s/『300』/Jeremiah/g
+s/『310』/Lamentations/g
+s/『330』/Ezekiel/g
+s/『340』/Daniel/g
+s/『350』/Hosea/g
+s/『360』/Joel/g
+s/『370』/Amos/g
+s/『380』/Obadiah/g
+s/『390』/Jonah/g
+s/『400』/Micah/g
+s/『410』/Nahum/g
+s/『420』/Habakkuk/g
+s/『430』/Zephaniah/g
+s/『440』/Haggai/g
+s/『450』/Zechariah/g
+s/『460』/Malachi/g
+s/『470』/Matthew/g
+s/『480』/Mark/g
+s/『490』/Luke/g
+s/『500』/John/g
+s/『510』/Acts/g
+s/『520』/Romans/g
+s/『530』/1 Corinthians/g
+s/『540』/2 Corinthians/g
+s/『550』/Galatians/g
+s/『560』/Ephesians/g
+s/『570』/Philippians/g
+s/『580』/Colossians/g
+s/『590』/1 Thessalonians/g
+s/『600』/2 Thessalonians/g
+s/『610』/1 Timothy/g
+s/『620』/2 Timothy/g
+s/『630』/Titus/g
+s/『640』/Philemon/g
+s/『650』/Hebrews/g
+s/『660』/James/g
+s/『670』/1 Peter/g
+s/『680』/2 Peter/g
+s/『690』/1 John/g
+s/『700』/2 John/g
+s/『710』/3 John/g
+s/『720』/Jude/g
+s/『730』/Revelation/g
